509ad9e18da2a5eee9f057756d2a1e9cca835b1e
brcshakil1 <brcshakil72@gmail.com>
2023-09-06 11:02:00 +0600

    first commit of milestone 7

---
+++ js-concepts-for-react/fundamantal.js
@@ -0,0 +1,9 @@
+// 1. How to declare a variable using let and const
+
+const fatherName = 'Arnold';
+let season = 'rainy season';
+season = 'winter season';
+
+// 2. condition
+// 6 condition:=> >, <, >=, <=, !==, ===
+// multiple condition
